57218b089c1343ef358ed4199a7ae7c787698c77
Neutrovertido <russellcode.james@gmail.com>
2024-08-03 21:01:40 -0600

    Initialized bot on Discord

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use dotenvy::dotenv;
 use std::env;
 
-fn main() {
-    println!("🦀 LowSpecBotRust");
+use serenity::async_trait;
+// use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    println!("🦀 LowSpecBot: Rust Edition");
     println!("⚙ Configuring environment variables...");
+
+    // This block attempts to use .env first and regular environment variables if it fails
     match dotenv() {
         Ok(_) => {
             println!("✅.env found! Utilizing its environment variables!")
@@ -14,5 +26,16 @@ fn main() {
         }
     }
     let token = env::var("TOKEN").expect("❗Discord token environment variable not found! Quitting program...\n");
-    println!("Token: {}", token);
+    
+    let intents = GatewayIntents::GUILD_MESSAGES
+    | GatewayIntents::DIRECT_MESSAGES
+    | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.expect("❗Error creating client");
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("Client error: {why:?}");
+    }
 }
