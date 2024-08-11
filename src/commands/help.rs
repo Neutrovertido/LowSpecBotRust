005c778c2beb13682715f91f8cf87e5bd0128358
Neutrovertido <russellcode.james@gmail.com>
2024-08-10 18:11:32 -0600

    Help command done

--- src/commands/help.rs
+++ src/commands/help.rs
@@ -1,9 +1,39 @@
 use crate::{Context, Error};
+use poise::CreateReply;
+use serenity::builder::CreateEmbed;
+use serenity::model::Timestamp;
 
 /// View available commands
 #[poise::command(slash_command)]
 pub async fn help(ctx: Context<'_>) -> Result<(), Error> {
-  //ctx.say("Pong!").await?;
-  
-  Ok(())
+    //ctx.say("Pong!").await?;
+    let commands = vec![
+        ("ping", "A ping command"),
+        ("8ball", "Iluminates your soul with some random generated phrases")
+    ];
+
+    let mut embed = CreateEmbed::default()
+        .title("Current Commands")
+        .description("Here are the available commands:")
+        .color(0)
+        .timestamp(Timestamp::now());
+
+    for (name, description) in commands {
+        embed = embed.field(name, description, false);
+    }
+
+    let reply = CreateReply {
+        content: None,
+        embeds: vec![embed],
+        attachments: vec![],
+        ephemeral: None,
+        components: None,
+        allowed_mentions: None,
+        reply: true,
+        __non_exhaustive: (),
+    };
+
+    ctx.send(reply).await?;
+    
+    Ok(())
 }
